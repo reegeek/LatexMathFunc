81c0f640c0685f93753601503d2f03074ace1310
Reegeek <10356780+reegeek@users.noreply.github.com>
2024-06-05 16:53:25 +0200

    [Antlr] First version with Antlr4BuildTasks and example of  AntlrDiscountPlatform

---
+++ src/MathFunc/LatexExpressionParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4.Runtime;
+
+using LatexGrammar;
+
+namespace MathFunc
+{
+    public class LatexExpressionParser
+    {
+        public void Parse(string latexExpression)
+        {
+            var charStream = new AntlrInputStream(latexExpression);
+            var lexer = new LatexLexer(charStream);
+            var tokenStream = new CommonTokenStream(lexer);
+            var parser = new LatexParser(tokenStream);
+            var tree = parser.rules();
+        }
+    }
+}
